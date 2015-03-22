8fdbb263575f5dcd2cef49127bcc16eaeeb87ea4
Jerry Evenson <jerry@getevenson.com>
2015-03-22 15:34:51 -0600

    Update changes for Assn 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script utilizes lexical scoping to cache data outside of a
+## function in the global environment where it can be reused.
 
-## Write a short comment describing this function
+## Establish a vector m that is the inverse of a provided matrix x
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Check for the existence of a cached matrix and if not found
+## use the solve function to generate one and cache it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
